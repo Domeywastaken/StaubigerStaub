da5978525a97165853b85d52b63967fd99fcd48e
DSu <dominik.suchanek@students.tbs1.de>
2021-04-28 14:59:23 +0200

    .py bearbeitet

--- tabelleauslesen.py
+++ tabelleauslesen.py
@@ -1,6 +1,12 @@
 import csv
+from pathlib import Path
+
+data_folder = Path("StaubigerStaub")
+
+file_to_open = data_folder / "20210426bme280sensor42069.csv" 
 
 
-reader = csv.reader(open("C:/Users/Domi/Documents/Python/2021-04-26_bme280_sensor_42069.csv"), delimiter=";")     
+
+reader = csv.reader(open(file_to_open), delimiter=";")     
 for row in reader:
    print("Zeit:", row[5],"Temperatur:", row[10])     
